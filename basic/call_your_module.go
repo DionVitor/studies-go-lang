86006938e29bf158b881cb5db093d932c3effdc2
dionvitor <dionvictor11@gmail.com>
2021-08-28 23:28:17 -0300

    Added error handling.

--- basic/call_your_module.go
+++ basic/call_your_module.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"my_module"
 )
 
 func main() {
-	message := my_module.Hello("Dion")
+	log.SetPrefix("String manipulator: ")
+	log.SetFlags(0) // Disable printing time, source file and line number in log.
+
+	message, err := my_module.Hello("")
+	if err != nil {
+		log.Fatal(err) // Print error and stop the program
+	}
+
 	fmt.Println(message)
 }
--- my_module/string_manipulator.go
+++ my_module/string_manipulator.go
@@ -1,9 +1,16 @@
 package my_module
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello it's function name, and start with capital letter because who call this function not in this package.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Hello %v", name)
-	return message
+	return message, nil
 }
