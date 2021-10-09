fbd3dcb183fe8a2d662ea427af1e72531d8935c1
dionvitor <dionvictor11@gmail.com>
2021-10-09 11:30:45 -0300

    Added study: stacks with defers.

--- basic/defer.go
+++ basic/defer.go
@@ -3,7 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("World!")
+	//defer fmt.Println("World!")
+	//
+	//fmt.Printf("Hello ")
+	stackingDefers()
+}
 
-	fmt.Printf("Hello ")
+func stackingDefers() {
+	fmt.Println("counting")
+
+	// Deferred functions calls are pushed onto a stack.
+	// When a function returns, it deferred calls are executed in last-in-first-out order.
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
 }
